b9a852971c8c760989507abc1c6dfe5d8a139371
Umber Bund <umberbun@un-a4.kzn.21-school.ru>
2024-07-21 20:38:32 +0300

    ладно, завтра продолжу, добавил выпадающий список

--- app/src/main/java/com/vktest/vktest/presentation/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/vktest/vktest/presentation/ui/fragments/MainFragment.kt
@@ -5,7 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import com.vktest.vktest.R
 import com.vktest.vktest.app.MyApp
 import com.vktest.vktest.databinding.FragmentMainBinding
 import com.vktest.vktest.presentation.ui.mainactivity.MainViewModel
@@ -13,6 +18,9 @@ import javax.inject.Inject
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
+    private lateinit var spinner: Spinner
+    private lateinit var valutesAdapter: ArrayAdapter<String>
+
     @Inject lateinit var viewModel: MainViewModel
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,6 +38,26 @@ class MainFragment : Fragment() {
         Log.d("MyTag", "i am a main fragment")
         Log.d("MyTag", viewModel.valutes.value.toString())
 
+        createSpinner()
+        setValutes()
+
+
+    }
+
+    private fun createSpinner(){
+        spinner = binding.spinner
+        valutesAdapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_item)
+        valutesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = valutesAdapter
+    }
+
+    private fun setValutes(){
+        viewModel.valutes.observe(viewLifecycleOwner, Observer { valutes ->
+            valutes?.let {
+                valutesAdapter.clear()
+                valutesAdapter.addAll(it.valutes.map { it.key })
+            }
+        })
     }
 
 }
